7678537463b8ec9f5468528fe6f0c0986c1b64ef
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-06-08 18:01:17 +0100

    initial commit

---
+++ ruby_arrays_and_hashes_quiz_tests.rb
@@ -0,0 +1,40 @@
+lines = ['Gyle Centre', 'Edinburgh Park', 'Murrayfield Stadium', 'Haymarket', 'Princes Street']
+
+puts lines.length
+
+lines[1]
+
+#haven't answered 3 yet
+
+lines.index("Haymarket")
+
+lines.unshift("Airport")
+
+lines.shift("York Place")
+
+lines.delete("Edinburgh Park")
+
+lines.delete_at(1)
+
+lines.reverse
+
+for stops in lines
+  puts stops
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
